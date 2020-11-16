eaabf89a2134b82007f6f012749a02b17a811e76
Lucas Tidas <Lucas@pc-225.home>
2020-11-16 17:38:30 +0100

    picture model and route deleted

--- models/Picture.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const Picture = mongoose.model("Picture", {
-  name: String,
-  picture: { type: mongoose.Schema.Types.Mixed, default: {} },
-});
-
-module.exports = Picture;
